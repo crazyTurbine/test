7a67bca5582e70a22f5c31e49dbf3b3f5434c711
akhkhayrullin <azat-meh@mail.ru>
2018-05-27 18:25:43 +0300

    добавлено java-класса

---
+++ src/com/level1/HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.level1;
+
+/**
+ * Created by akhkhayrullin on 27.05.2018.
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld1");
+    }
+}
